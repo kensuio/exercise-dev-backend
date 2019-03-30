d476cbf25a007809a165fd3d2fb013a7ec2b6fe2
oleksii-prosianko <oleksii.prosianko@nezasa.com>
2019-03-30 20:39:05 +0200

    olelksii-prosyanko: GET /users/{id} implemented

--- users/src/main/scala/users/api/JsonSupport.scala
+++ users/src/main/scala/users/api/JsonSupport.scala
@@ -8,8 +8,9 @@ trait JsonSupport extends SprayJsonSupport {
   import DefaultJsonProtocol._
   import UserApiRoutes._
 
-  implicit val userDataFormat = jsonFormat6(UserData)
+  implicit val userDataFormat = jsonFormat6(UserData.apply)
   implicit val signUpDataFormat = jsonFormat3(SignUpData)
   implicit val passwordDataFormat = jsonFormat1(PasswordData)
   implicit val statusDataFormat = jsonFormat1(StatusData)
+  implicit val failFormat = jsonFormat1(Fail)
 }
--- users/src/main/scala/users/api/UserApiRoutes.scala
+++ users/src/main/scala/users/api/UserApiRoutes.scala
@@ -1,14 +1,16 @@
 package users.api
 
-import akka.http.scaladsl.server.Route
-import users.services.UserManagement
-import scala.concurrent.Future
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.directives.MethodDirectives.get
 import akka.http.scaladsl.server.directives.PathDirectives.path
 import akka.http.scaladsl.server.directives.RouteDirectives.complete
+import users.domain.User
+import users.services.UserManagement
+import users.services.usermanagement.Error
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
 
 object UserApiRoutes {
 
@@ -21,6 +23,17 @@ object UserApiRoutes {
       version: Int
   )
 
+  object UserData {
+    def apply(u: User): UserData = UserData(
+      id = u.id.value,
+      name = u.userName.value,
+      email = u.emailAddress.value,
+      status = u.status.toString,
+      password = u.password.map(_.toString),
+      version = u.metadata.version
+    )
+  }
+
   final case class SignUpData(
       name: String,
       email: String,
@@ -29,9 +42,13 @@ object UserApiRoutes {
 
   final case class PasswordData(password: Option[String])
   final case class StatusData(status: String)
+
+  case class Fail(err: String)
 }
 
 trait UserApiRoutes extends JsonSupport {
+
+  import UserApiRoutes._
 
   val UserVersionHeader = "User-Version"
 
@@ -41,7 +58,21 @@ trait UserApiRoutes extends JsonSupport {
 
   lazy val routes: Route = path("api" / apiVersion / "users"/ Segment) { id =>
     get {
-      complete("Hello")
+      onComplete(userManagement.get(User.Id(id))) {
+        case Success(Right(u)) => complete((StatusCodes.OK, UserData(u)))
+        case Success(Left(err)) => err match {
+          case Error.NotFound => complete((
+            StatusCodes.NotFound,
+            Fail(s"No user found for id: $id")
+          ))
+          case _ => complete((
+            StatusCodes.InternalServerError, Fail("Unknown Server error")))
+        }
+        case Failure(ex) => complete((
+          StatusCodes.InternalServerError,
+          Fail(ex.getMessage)
+        ))
+      }
     }
   }
 }
